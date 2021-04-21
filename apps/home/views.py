598f82467d47a18c038349abd909238a16fc8668
GuillaumeM92 <gemacx@gmail.com>
2021-04-21 11:56:42 +0200

    added language tags in html

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def about(request):
-    return render(request, 'home/about.html')
+    return render(request, 'home/about.html', {'title': 'À mon sujet'})
 
 def portfolio(request):
-    return render(request, 'home/portfolio.html')
+    return render(request, 'home/portfolio.html', {'title': 'Mes Projets'})
 
 def about_en(request):
-    return render(request, 'home/about_en.html')
+    return render(request, 'home/about_en.html', {'title': 'About Me'})
 
 def portfolio_en(request):
-    return render(request, 'home/portfolio_en.html')
+    return render(request, 'home/portfolio_en.html', {'title': 'Portfolio'})
